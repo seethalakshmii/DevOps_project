77e6fe509f248619ed016efdc3d2fb1a7093905f
seetha <seethalakshmikap@gmail.com>
2026-06-12 15:49:58 +0530

    testing

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,7 @@
-output "ec2_public_ip" {
-  value       = aws_instance.app.public_ip
-  description = "Public IP of EC2 instance"
+output "instance_id" {
+  value = aws_instance.app.id
+}
+
+output "public_ip" {
+  value = aws_instance.app.public_ip
 }
