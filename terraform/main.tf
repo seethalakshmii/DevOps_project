761ac5ead5a21e554c781c834ef1102440e777b2
seetha <seethalakshmikap@gmail.com>
2026-06-12 14:39:27 +0530

    changed terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,33 +40,6 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-# -------------------------
-# IAM ROLE FOR SSM
-# -------------------------
-resource "aws_iam_role" "ec2_ssm_role" {
-  name = "ec2-ssm-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_policy" {
-  role       = aws_iam_role.ec2_ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-ssm-profile"
-  role = aws_iam_role.ec2_ssm_role.name
-}
 
 # -------------------------
 # EC2 INSTANCE
